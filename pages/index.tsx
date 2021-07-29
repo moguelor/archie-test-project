996711322e1116ebe31d68c31525cfb90d123b37
Jose Obed Moguel Ruiz <jmoguelruiz@gmail.com>
2021-07-29 00:44:32 -0500

    Refactored structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,57 @@
-import * as home from "./home";
-import { client } from "./apolloConfig";
-import { LAUNCHES_PAST_SEARCH_QUERY } from "./home/querys";
+import { client } from "../packages/configs/apolloConfig";
+import { LAUNCHES_PAST_SEARCH_QUERY } from "../packages/shared/querys";
+import { useState } from "react";
+import {
+  LaunchedList,
+  SearchInput,
+  Loader,
+  ErrorMessage,
+} from "../packages/shared/components";
+import { Container } from "@chakra-ui/react";
+import { useLazyQuery } from "@apollo/client";
+import { LaunchedItem } from "../packages/shared/types";
+
+type HomeProps = {
+  launchesPast?: LaunchedItem[];
+};
+
+const Home = ({ launchesPast }: HomeProps) => {
+  const [text, setText] = useState("");
+  const [runQuery, { data, loading, error }] = useLazyQuery(
+    LAUNCHES_PAST_SEARCH_QUERY
+  );
+
+  const filteredData = data ? data.launchesPast : launchesPast;
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    runQuery({
+      variables: { filter: text },
+    });
+  };
+
+  return (
+    <Container
+      paddingTop="35px"
+      paddingBottom="35px"
+      maxW="110ch"
+      centerContent
+    >
+      <SearchInput
+        handleSubmit={handleSubmit}
+        handleChange={(text: string) => setText(text)}
+      />
+
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <ErrorMessage text={error && error.message} />
+      ) : (
+        <LaunchedList data={filteredData} />
+      )}
+    </Container>
+  );
+};
 
 export async function getStaticProps() {
   const { data } = await client.query({
@@ -13,4 +64,5 @@ export async function getStaticProps() {
     },
   };
 }
-export default home.Container;
+
+export default Home;
